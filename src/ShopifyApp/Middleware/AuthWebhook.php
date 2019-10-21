03b78d7fa3f6a025e4c58325b15da1a2952f5e84
Dario Sande <dsande@mindresearch.org>
2019-10-21 16:55:31 -0700

    creating shop session when in a webhook

--- src/ShopifyApp/Middleware/AuthWebhook.php
+++ src/ShopifyApp/Middleware/AuthWebhook.php
@@ -5,7 +5,9 @@ namespace OhMyBrew\ShopifyApp\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Config;
 use OhMyBrew\ShopifyApp\Facades\ShopifyApp;
+use OhMyBrew\ShopifyApp\Services\ShopSession;
 
 /**
  * Response for ensuring a proper webhook request.
@@ -33,7 +35,43 @@ class AuthWebhook
             return Response::make('Invalid webhook signature.', 401);
         }
 
+        $validation = $this->validateShop($request);
+        if ($validation !== true) {
+            return Response::make('Invalid shop.', 401);
+        }
+
         // All good, process webhook
         return $next($request);
     }
+
+    /**
+     * Checks we have a valid shop.
+     *
+     * @param \Illuminate\Http\Request $request The request object.
+     *
+     * @return bool|\Illuminate\Http\RedirectResponse
+     */
+    protected function validateShop(Request $request)
+    {
+        // Setup the session service
+        $session = new ShopSession();
+
+        $shopDomain = $request->header('x-shopify-shop-domain');
+
+        // Get the shop based on domain and update the session service
+        $shopModel = Config::get('shopify-app.shop_model');
+        $shop = $shopModel::withTrashed()
+            ->where(['shopify_domain' => $shopDomain])
+            ->first();
+        $session->setShop($shop);
+
+        // We need to do a full flow if no shop or it is deleted
+        if ($shop === null || $shop->trashed() || !$session->isValid()) {
+            return false;
+        }
+
+        // Everything is fine!
+        return true;
+    }
+
 }
